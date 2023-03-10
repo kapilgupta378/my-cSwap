1a92c9ce9c752cdb01c4a6d414a4f90866f14083
Kapil <kapil@weblliasto.com>
2023-03-10 21:10:35 +0530

    header active added

--- src/components/layout/header/index.js
+++ src/components/layout/header/index.js
@@ -1,23 +1,38 @@
-import React from "react";
+import React, { use } from "react";
 import styles from "./styles.module.scss";
 import Button from "../../common/button";
 import Link from "next/link";
 import Image from "next/image";
 import logoIcon from "../../../../public/assets/images/cSwap LIGHT.svg";
+import { useRouter } from "next/router";
 const Header = () => {
+  const router = useRouter();
   return (
     <div className={styles.header_wrap}>
       <div>
         <Image src={logoIcon} width={80} height={30} alt="logo" />
       </div>
       <div className={styles.route_wrap}>
-        <Link className={styles.route_link} href={"/"}>
+        <Link
+          style={{ color: router.pathname === "/" ? "#73aed2" : "" }}
+          className={styles.route_link}
+          href={"/"}
+        >
           Overview
         </Link>
-        <Link className={styles.route_link} href={"/pools"}>
+
+        <Link
+          style={{ color: router.pathname === "/pools" ? "#73aed2" : "" }}
+          className={styles.route_link}
+          href={"/pools"}
+        >
           Pools
         </Link>
-        <Link className={styles.route_link} href={"/tokens"}>
+        <Link
+          style={{ color: router.pathname === "/tokens" ? "#73aed2" : "" }}
+          className={styles.route_link}
+          href={"/tokens"}
+        >
           Tokens
         </Link>
       </div>
