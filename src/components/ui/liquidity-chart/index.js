a2a9ee71fcce52560b34e1e47d97d93624e1b40e
Kapil <kapil@weblliasto.com>
2023-03-10 21:10:54 +0530

    liquidity chart added

---
+++ src/components/ui/liquidity-chart/index.js
@@ -0,0 +1,102 @@
+import React from "react";
+import Background from "../../common/background";
+import styles from "./styles.module.scss";
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+} from "recharts";
+// const data = [
+//   {
+//     name: "Page A",
+//     uv: 4000,
+//     pv: 2400,
+//     amt: 2400,
+//   },
+//   {
+//     name: "Page B",
+//     uv: 3000,
+//     pv: 1398,
+//     amt: 2210,
+//   },
+//   {
+//     name: "Page C",
+//     uv: 2000,
+//     pv: 9800,
+//     amt: 2290,
+//   },
+//   {
+//     name: "Page D",
+//     uv: 2780,
+//     pv: 3908,
+//     amt: 2000,
+//   },
+//   {
+//     name: "Page E",
+//     uv: 1890,
+//     pv: 4800,
+//     amt: 2181,
+//   },
+//   {
+//     name: "Page F",
+//     uv: 2390,
+//     pv: 3800,
+//     amt: 2500,
+//   },
+//   {
+//     name: "Page G",
+//     uv: 3490,
+//     pv: 4300,
+//     amt: 2100,
+//   },
+// ];
+const LiquidityChart = ({
+  data,
+  YAxisDataKey,
+  XAxisDataKey,
+  areaDataKey,
+  liquidity = 0,
+}) => {
+  return (
+    <Background>
+      <div className={styles.header}>
+        <div>
+          <p>Liquidity</p>
+          <h2>${liquidity}</h2>
+        </div>
+        <div></div>
+      </div>
+
+      <div className={styles.area_chart_wrap}>
+        <AreaChart
+          id="AreaChart "
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 0,
+            right: 0,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <XAxis id="XAxis" dataKey={XAxisDataKey} tickCount={40} />
+          <YAxis id="YAxis" dataKey={YAxisDataKey} tickCount={40} />
+          <Tooltip />
+          <Area
+            id="Area"
+            type="monotone"
+            dataKey={areaDataKey}
+            stroke="#007bff"
+            fill="#374e66"
+          />
+        </AreaChart>
+      </div>
+    </Background>
+  );
+};
+
+export default LiquidityChart;
